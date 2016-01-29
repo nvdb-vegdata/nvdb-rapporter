25535602fcd7a9f046431436e39cff8385d71071
Magnus Haugsand <haugsand@gmail.com>
2016-01-29 09:44:02 +0100

    Commit

---
+++ api.php
@@ -0,0 +1,34 @@
+﻿<?php 
+
+function getData($path) {
+    $path = str_replace("#", "%23", $path);
+    $path = str_replace("+", "%2B", $path);
+    $path = str_replace("Æ", "%C3%86", $path);
+    $path = str_replace("æ", "%C3%A6", $path);
+    $path = str_replace("Ø", "%C3%98", $path);
+    $path = str_replace("ø", "%C3%B8", $path);
+    $path = str_replace("Å", "%C3%85", $path);
+    $path = str_replace("å", "%C3%A5", $path);
+    $path = str_replace(" ", "%20", $path);
+    $url = 'https://www.vegvesen.no/nvdb/api'.$path;
+
+    $options = array(
+        'http'=>array(
+        'method'=>"GET",
+        'header'=>"Accept: application/vnd.vegvesen.nvdb-v1+json"
+        )
+    );
+    $context = stream_context_create($options);
+      
+    $output = file_get_contents($url, false, $context); 
+      
+    return $output;
+}
+
+if (isset($_GET['path'])) {
+    $data = getData($_GET['path']);
+    print_r($data);
+}
+
+
+?>
---
+++ js/directives.js
@@ -0,0 +1,25 @@
+﻿app.directive('nvdbRapport', function() {
+
+    return {
+        restrict: 'E',
+        templateUrl: 'rapport.html'
+    };
+});
+
+var FLOAT_REGEXP = /^\-?\d+((\.|\,)\d+)?$/;
+app.directive('smartFloat', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, elm, attrs, ctrl) {
+      ctrl.$parsers.unshift(function(viewValue) {
+        if (FLOAT_REGEXP.test(viewValue)) {
+          ctrl.$setValidity('float', true);
+          return parseFloat(viewValue.replace(',', '.'));
+        } else {
+          ctrl.$setValidity('float', false);
+          return undefined;
+        }
+      });
+    }
+  };
+});
